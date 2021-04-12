cb2fc8e1f69d5059fc275926d89030d3f7f65c1d
Erdal Dogan <erdalsidal99@gmail.com>
2021-04-12 17:47:56 +0300

    get diff anlgle

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,6 @@
 import simplekml
 from polycircles import polycircles
-from utilfunctions import haversine, fresnel_radius, get_circle_coords
+from utilfunctions import *
 
 
 
@@ -8,6 +8,7 @@ def main():
     mef = (29.008578, 41.108460, 200)
     camlica = (29.068520, 41.026062, 265)
     D = haversine(mef[0], mef[1], camlica[0], camlica[1])
+    diff = get_diff_angle(mef, camlica)
 
     kml = simplekml.Kml()
     kml.newpoint(name="MEF University", coords=[mef])  # lon, lat, optional height
@@ -17,12 +18,12 @@ def main():
                             coords=[mef, camlica])
     #los.extrude = 1
     los.altitudemode = simplekml.AltitudeMode.absolute
-    los.style.linestyle.width = 1
-    los.style.linestyle.color = simplekml.Color.blue
+    los.style.linestyle.width = 4
+    los.style.linestyle.color = simplekml.Color.pink
 
     pol = kml.newpolygon(name='A Polygon', altitudemode=simplekml.AltitudeMode.absolute)
-    pol.outerboundaryis = [(29.007434, 41.106582, 100), (29.011761, 41.106916, 100),
-                           (29.010320, 41.110875, 100), (29.006149, 41.110221, 100)]
+    #pol.outerboundaryis = [(29.007434, 41.106582, 100), (29.011761, 41.106916, 100), (29.010320, 41.110875, 100), (29.006149, 41.110221, 100)]
+    pol.outerboundaryis = get_circle_coords(mef, 10, diff, 36)
     pol.style.linestyle.color = simplekml.Color.green
     pol.style.linestyle.width = 5
     pol.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.green)
--- src/utilfunctions.py
+++ src/utilfunctions.py
@@ -1,4 +1,4 @@
-from math import radians, cos, sin, asin, sqrt
+from math import radians, cos, sin, asin, sqrt, pi, atan
 
 def haversine(lon1, lat1, lon2, lat2):
     """
@@ -21,16 +21,24 @@ def fresnel_radius(n, freq, d1, d2):
     return sqrt((n * freq * d1 * d2)/(d1 + d2))
 
 
-def get_circle_coords(coord, radius, tilt, vertex_no):
+def get_diff_angle(coord1, coord2):
+    d_lat = abs(coord1[1] - coord2[1])
+    d_lon = abs(coord1[0] - coord2[0])
+    return atan((d_lat/d_lon)) * 180/pi  #rad to deg conv
+
+
+def get_circle_coords(coord, radius, diff, vertex_no):
     lat = coord[0]
     lon = coord[1]
+    h = coord[2]
     coord = []
     degree_step = 360.0/vertex_no
     d=0
     while (d < 360.0):
-        delta_x = sin(d) * radius
-        delta_y = radius * cos(d)
-        coord.append((lat + delta_x, lon + delta_y))
+        delta_lat = sin(d * pi/180) * radius / 111111
+        delta_long = 0
+        delta_h = cos(d * pi/180) * radius
+        coord.append((lat + delta_lat, lon + delta_long, h + delta_h))
         d += degree_step
     return coord
 
