ec5a7291e83146f2422665d0a4da392a3296ec0b
Erdal Dogan <erdalsidal99@gmail.com>
2021-04-14 11:32:04 +0300

    draw fresnel zone

--- src/Main.py
+++ src/Main.py
@@ -1,13 +1,14 @@
 import simplekml
-from polycircles import polycircles
 from utilfunctions import *
 
 
 
 def main():
-    mef = (29.008578, 41.108460, 200)
+    STEP_SIZE = 500
+    f = 1900 * 10**5
+    mef = (29.008578, 41.108460, 180)
     camlica = (29.068520, 41.026062, 265)
-    D = haversine(mef[0], mef[1], camlica[0], camlica[1])
+    D = haversine(mef[0], mef[1], mef[2], camlica[0], camlica[1], camlica[2])
     diff = get_diff_angle(mef, camlica)
 
     kml = simplekml.Kml()
@@ -19,16 +20,27 @@ def main():
     #los.extrude = 1
     los.altitudemode = simplekml.AltitudeMode.absolute
     los.style.linestyle.width = 4
-    los.style.linestyle.color = simplekml.Color.pink
+    los.style.linestyle.color = simplekml.Color.red
 
-    pol = kml.newpolygon(name='A Polygon', altitudemode=simplekml.AltitudeMode.absolute)
-    #pol.outerboundaryis = [(29.007434, 41.106582, 100), (29.011761, 41.106916, 100), (29.010320, 41.110875, 100), (29.006149, 41.110221, 100)]
-    pol.outerboundaryis = get_circle_coords(mef, 10, diff, 36)
-    pol.style.linestyle.color = simplekml.Color.green
-    pol.style.linestyle.width = 5
-    pol.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.green)
+    lat1, lon1 = mef[0], mef[1]
+    lat2, lon2 = camlica[0], camlica[1]
 
+    delta_lat = (lat1 - lat2) / STEP_SIZE
+    delta_long = (lon1 - lon2) / STEP_SIZE
+    delta_h = (mef[2] - camlica[2]) / STEP_SIZE
 
+    tmp = tuple()
+    delta_d = haversine(0, 0, 0, delta_long, delta_lat, delta_h)
+    for i in range(460):
+        tmp = ((delta_lat * i + lat2), (delta_long * i + lon2), (delta_h * i + camlica[2]))
+        d1 = delta_d * i
+        print(d1)
+        pol = kml.newpolygon(name='A Polygon', altitudemode=simplekml.AltitudeMode.absolute)
+        pol.outerboundaryis = get_circle_coords(coord=tmp, radius=fresnel_radius(1,freq=f, d1=d1, d2=D-d1),
+                                                diff=diff, vertex_no=360)
+        pol.style.linestyle.color = simplekml.Color.darkblue
+        pol.style.linestyle.width = 5
+        pol.style.polystyle.color = simplekml.Color.changealphaint(0, simplekml.Color.rgb(0,0,0,0))
 
     kml.save("fresnel-zone.kml")
 if __name__ == "__main__":
--- src/utilfunctions.py
+++ src/utilfunctions.py
@@ -1,24 +1,27 @@
 from math import radians, cos, sin, asin, sqrt, pi, atan
 
-def haversine(lon1, lat1, lon2, lat2):
+c = 3 * 10**8
+
+def haversine(lon1, lat1, h1, lon2, lat2, h2):
     """
     Calculate the great circle distance between two points
     on the earth (specified in decimal degrees)
     """
     # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-
+    d_h = abs(h1 - h2) / 1000
     # haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a))
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    return c * r
+    return sqrt((c * r)**2 + d_h**2) * 1000
 
 
 def fresnel_radius(n, freq, d1, d2):
-    return sqrt((n * freq * d1 * d2)/(d1 + d2))
+    lam = (c) / freq
+    return sqrt((n * lam * d1 * d2)/(d1 + d2))
 
 
 def get_diff_angle(coord1, coord2):
@@ -34,7 +37,7 @@ def get_circle_coords(coord, radius, diff, vertex_no):
     coord = []
     degree_step = 360.0/vertex_no
     d=0
-    while (d < 360.0):
+    while (d <= 360.0):
         delta_lat = sin(d * pi/180) * radius / 111111
         delta_long = 0
         delta_h = cos(d * pi/180) * radius
