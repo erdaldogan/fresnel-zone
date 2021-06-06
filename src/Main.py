3e00567e7a72e1b8cfde152eb08ae6b87366b8cc
Erdal Dogan <erdalsidal99@gmail.com>
2021-06-06 13:40:05 +0300

    updates

--- src/Main.py
+++ src/Main.py
@@ -1,15 +1,40 @@
 import simplekml
 from utilfunctions import *
-
+from geopy.distance import great_circle
 
 
 def main():
-    STEP_SIZE = 500
-    f = 1900 * 10**5
-    mef = (29.008578, 41.108460, 180)
-    camlica = (29.068520, 41.026062, 265)
-    D = haversine(mef[0], mef[1], mef[2], camlica[0], camlica[1], camlica[2])
+    f = 1900 * 10 ** 5
+
+    mef = (29.008578, 41.108460, 80)
+    camlica = (29.068520, 41.026062, 270)
+
+
+    #mef = (29.0484, 41.022, 1780)
+    #camlica = (35.334415, 36.995934, 23)
+    #camlica = (29.0664, 41.0148, 150)
+
+    distance = sqrt((great_circle(mef, camlica).m)**2 + (mef[2] - camlica[2])**2)  # great_circle returns 2d distance
+    STEP_SIZE = int(distance / 20 + 1)
+    ALTITUDE_STEP = (mef[2] - camlica[2]) / STEP_SIZE
+    print("DISTNACE", distance)
+
+    difference = (mef[0] - camlica[0]), (mef[1] - camlica[1]), (mef[2] - camlica[2])
+
+    step_dist = distance / STEP_SIZE
+    unit_vector = difference[0] / STEP_SIZE, difference[1] / STEP_SIZE, difference[2] / STEP_SIZE
+
+    #step_coords = unit_vector[0] * step_dist, unit_vector[1] * step_dist, unit_vector[2] * step_dist
+    step_coords = unit_vector
+
+    print("STEP DIST", step_dist)
+    print("UNIT VECTOR", unit_vector)
+    print("STEP COORDS", step_coords)
+
+    current_point = mef
+
     diff = get_diff_angle(mef, camlica)
+    print("DIFF", diff)
 
     kml = simplekml.Kml()
     kml.newpoint(name="MEF University", coords=[mef])  # lon, lat, optional height
@@ -17,31 +42,27 @@ def main():
 
     los = kml.newlinestring(name="Line of Sight", description="LOS between given points",
                             coords=[mef, camlica])
-    #los.extrude = 1
+    # los.extrude = 1
     los.altitudemode = simplekml.AltitudeMode.absolute
     los.style.linestyle.width = 4
     los.style.linestyle.color = simplekml.Color.red
 
-    lat1, lon1 = mef[0], mef[1]
-    lat2, lon2 = camlica[0], camlica[1]
-
-    delta_lat = (lat1 - lat2) / STEP_SIZE
-    delta_long = (lon1 - lon2) / STEP_SIZE
-    delta_h = (mef[2] - camlica[2]) / STEP_SIZE
-
-    tmp = tuple()
-    delta_d = haversine(0, 0, 0, delta_long, delta_lat, delta_h)
-    for i in range(460):
-        tmp = ((delta_lat * i + lat2), (delta_long * i + lon2), (delta_h * i + camlica[2]))
-        d1 = delta_d * i
-        print(d1)
+    for i in range(STEP_SIZE):
         pol = kml.newpolygon(name='A Polygon', altitudemode=simplekml.AltitudeMode.absolute)
-        pol.outerboundaryis = get_circle_coords(coord=tmp, radius=fresnel_radius(1,freq=f, d1=d1, d2=D-d1),
-                                                diff=diff, vertex_no=360)
+        d1 = sqrt((great_circle(mef, current_point).m)**2 + (mef[2] - current_point[2])**2)
+        #print(distance, d1)
+
+        pol.outerboundaryis = get_circle_coords(coord=current_point,
+                                                radius=fresnel_radius(1, freq=f, d1=d1, d2=distance - d1),
+                                                diff_angle=diff, vertex_no=360)
         pol.style.linestyle.color = simplekml.Color.darkblue
         pol.style.linestyle.width = 5
-        pol.style.polystyle.color = simplekml.Color.changealphaint(0, simplekml.Color.rgb(0,0,0,0))
+        pol.style.polystyle.color = simplekml.Color.changealphaint(0, simplekml.Color.rgb(0, 0, 0, 0))
+        current_point = current_point[0] - step_coords[0], current_point[1] - step_coords[1], current_point[2] - \
+                        ALTITUDE_STEP
 
     kml.save("fresnel-zone.kml")
+
+
 if __name__ == "__main__":
     main()
--- src/utilfunctions.py
+++ src/utilfunctions.py
@@ -1,4 +1,4 @@
-from math import radians, cos, sin, asin, sqrt, pi, atan
+from math import radians, cos, sin, asin, sqrt, pi, atan2
 
 c = 3 * 10**8
 
@@ -26,22 +26,38 @@ def fresnel_radius(n, freq, d1, d2):
 
 def get_diff_angle(coord1, coord2):
     d_lat = abs(coord1[1] - coord2[1])
-    d_lon = abs(coord1[0] - coord2[0])
-    return atan((d_lat/d_lon)) * 180/pi  #rad to deg conv
+    d_lon = abs(coord2[0] - coord1[0])
+    y = sin(d_lon) * cos(coord2[1])
+    x = cos(coord1[1]) * sin(coord2[1]) - sin(coord1[1]) * cos(coord2[1]) * cos(d_lon)
+    return atan2(y, x) * 180.0/pi  #rad to deg conv
+
+def rotate_point(point, origin, angle):
+    angle_rad = angle * pi/180.0
+    (px, py) = point
+    (ox, oy, _) = origin
+    x = cos(angle_rad) * (px - ox) - sin(angle_rad) * (py - oy) + ox
+    y = sin(angle_rad) * (px - ox) + cos(angle_rad) * (py - oy) + oy
+    return x, y
 
 
-def get_circle_coords(coord, radius, diff, vertex_no):
+def get_circle_coords(coord, radius, diff_angle, vertex_no):
     lat = coord[0]
     lon = coord[1]
     h = coord[2]
-    coord = []
+    coord_out = []
     degree_step = 360.0/vertex_no
     d=0
-    while (d <= 360.0):
+    # long = horizontal
+    # lat = vertical
+    while d <= 360.0:
         delta_lat = sin(d * pi/180) * radius / 111111
         delta_long = 0
         delta_h = cos(d * pi/180) * radius
-        coord.append((lat + delta_lat, lon + delta_long, h + delta_h))
+        (x, y) = rotate_point(point=(lat + delta_lat, lon + delta_long),
+                          origin=coord, angle=diff_angle)
+        coord_out.append((x, y, h + delta_h))
+        #coord_out.append((lat + delta_lat, lon + delta_long, h + delta_h))
+
         d += degree_step
-    return coord
+    return coord_out
 
